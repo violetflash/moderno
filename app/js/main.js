1c9289570676014c788164e21d585dfdbaf63c2c
Alexander Grobovsky <ktotaam@mail.ru>
2020-05-20 10:43:50 +0500

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,19 @@
 $(function () {
 
-
     var mixer = mixitup('.products__inner-box');
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+    });
+
+    $('.product-slider-inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true,
+        arrows: false,
+        infinite : false,
+    })
 
 });
