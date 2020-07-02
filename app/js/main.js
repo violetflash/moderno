36b55fccd8dabac161c933b344845ba47e228793
Alexander Grobovsky <ktotaam@mail.ru>
2020-07-02 11:29:38 +0500

    save

--- app/js/main.js
+++ app/js/main.js
@@ -12,7 +12,7 @@ $(function () {
         slidesToScroll: 4,
         dots: true,
         arrows: false,
-        infinite : false,
+        infinite: false,
     });
 
     $(".js-range-slider").ionRangeSlider({
@@ -37,24 +37,23 @@ $(function () {
     });
 
     $('.menu__btn').on('click', function () {
-       $('.menu__list').slideToggle()
+        $('.menu__list').slideToggle()
     });
 
     $('.header__btn-menu').on('click', function () {
-       $('.header__box').toggleClass('active')
+        $('.header__box').toggleClass('active')
     });
 
-    $('.product-one__tabs .tab').on('click', function(event) {
+    $('.product-one__tabs .tab').on('click', function (event) {
         var id = $(this).attr('data-id');
         $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
         $('.product-one__tabs .tabs').find('.tab').removeClass('active');
         $(this).addClass('active');
-        $('#'+id).addClass('active-tab').fadeIn();
+        $('#' + id).addClass('active-tab').fadeIn();
         return false;
     });
 
     var mixer = mixitup('.products__inner-box');
-
 
 
 });
